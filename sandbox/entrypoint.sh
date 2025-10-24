32d77318530f448ac0c12d3dc977ceb5b1c83a3d
Marek Sterzik <marek.sterzik@gmail.com>
2025-10-25 00:25:59 +0200

    initial commit of the sandbox

---
+++ sandbox/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p /persistent/ssh-keys/etc/ssh
+
+ssh-keygen -A -f /persistent/ssh-keys
+
+/usr/sbin/sshd
+
+if [ -n "$AUTHORIZED_KEYS" ]; then
+    echo "$AUTHORIZED_KEYS" > /home/sandbox/.ssh/authorized_keys
+    chown sandbox:sandbox /home/sandbox/.ssh/authorized_keys
+    chmod 0600 /home/sandbox/.ssh/authorized_keys
+else
+    rm -f /home/sandbox/.ssh/authorized_keys
+fi
+
+exec dockerd-entrypoint.sh "$@"

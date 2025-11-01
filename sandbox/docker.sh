f6d081a31f2a9bbeab43af902e3d7b307d578ab9
Marek Sterzik <marek.sterzik@gmail.com>
2025-11-01 16:58:09 +0100

    docker ci isolation option

--- sandbox/docker.sh
+++ sandbox/docker.sh
@@ -8,17 +8,31 @@ test_code() {
     test "$code" -lt 300 -a "$code" -ge 200
 }
 
-if [ -n "$CI_JOB_ID" -a -n "$CI_SERVER_URL" -a -n "$CI_JOB_TOKEN" ]; then
-    job_url="$CI_SERVER_URL/api/v4/job?job_token=$CI_JOB_TOKEN"
-    docker_config="/tmp/docker-config/job-$CI_JOB_ID"
-    if [ ! -d "$docker_config" ]; then
-        if test_code "$job_url"; then
-            mkdir -p "$docker_config"
-            echo "$job_url" > "$docker_config/job_url.conf"
+cleanup() {
+    find /tmp/docker-config -maxdepth 1 -mindepth 1 -mmin +60 | while read job_dir; do
+        job_url_file="$job_dir/job_url.conf"
+        if ! [ -f "$job_url_file" ] || ! test_code "`cat "$job_url_file"`"; then
+            rm -rf "$job_dir"
+        else
+            touch "$job_dir"
+        fi
+    done
+}
+
+if [ -z "$DOCKER_CONFIG" ]; then
+    cleanup
+    if [ -n "$CI_JOB_ID" -a -n "$CI_SERVER_URL" -a -n "$CI_JOB_TOKEN" ]; then
+        job_url="$CI_SERVER_URL/api/v4/job?job_token=$CI_JOB_TOKEN"
+        docker_config="/tmp/docker-config/job-$CI_JOB_ID"
+        if [ ! -d "$docker_config" ]; then
+            if test_code "$job_url"; then
+                mkdir -p "$docker_config"
+                echo "$job_url" > "$docker_config/job_url.conf"
+                export DOCKER_CONFIG="$docker_config"
+            fi
+        else
             export DOCKER_CONFIG="$docker_config"
         fi
-    else
-        export DOCKER_CONFIG="$docker_config"
     fi
 fi
 
--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
+load_bool() {
+    local val
+    val="$1"
+    if [ -z "$val" ]; then
+        val="$2"
+    fi
+    val="`echo "$val" | awk '{print tolower($0)}'`"
+    if [ "1" = "$val" -o "yes" = "$val" -o "true" = "$val" ]; then
+        echo 1
+    elif [ "0" = "$val" -o "no" = "$val" -o "false" = "$val" ]; then
+        echo 0
+    fi
+}
+
 if [ -n "$AUTHORIZED_KEYS" ]; then
     echo "$AUTHORIZED_KEYS" > /home/sandbox/.ssh/authorized_keys
     chown sandbox:sandbox /home/sandbox/.ssh/authorized_keys
     chmod 0600 /home/sandbox/.ssh/authorized_keys
 else
     rm -f /home/sandbox/.ssh/authorized_keys
+fi
+
+DOCKER_CI_ISOLATION="`load_bool "$DOCKER_CI_ISOLATION" "true"`"
+if [ -z "$DOCKER_CI_ISOLATION" ]; then
+    echo "Error: invalid value of DOCKER_CI_ISOLATION variable (<bool>)" 1>&2
+    exit 1
+fi
+
+if [ "$DOCKER_CI_ISOLATION" = 1 ]; then
+    rm -f /usr/local/bin/docker
+    ln -s docker.sh /usr/local/bin/docker
+else
+    rm -f /usr/local/bin/docker
+    ln -s docker.bin /usr/local/bin/docker
 fi
 
 /usr/sbin/sshd

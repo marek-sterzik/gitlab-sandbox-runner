1be5a6d5f892fbdcecac0c71b39c374682290b26
Marek Sterzik <marek.sterzik@gmail.com>
2025-11-01 01:47:32 +0100

    implement runner

--- runner/user-entrypoint.sh
+++ runner/user-entrypoint.sh
@@ -60,12 +60,20 @@ check_env() {
     fi
 
     if [ -z "$SANDBOX_LOAD_GITLAB_ENV" ]; then
-        echo "Error: invalid value of SANDBOX_LOAD_GITLAB_ENV" 1>&2
+        echo "Error: invalid value of SANDBOX_LOAD_GITLAB_ENV variable (true|false)" 1>&2
+        exit 1
+    fi
+
+    if echo "$GITLAB_URL" | grep -qv '^https\?://.*$' || [ "`echo "$GITLAB_URL" | wc -l`" -gt 1 ]; then
+        echo "Error: invalid value of GITLAB_URL variable" 1>&2
+        exit 1
+    fi
+
+    if echo "$SSH_CONNECT" | grep -qv '^.\+@.\+$' || [ "`echo "$SSH_CONNECT" | wc -l`" -gt 1 ]; then
+        echo "Error: invalid value of SSH_CONNECT variable (<user>@<host>[:<port>])" 1>&2
         exit 1
     fi
 }
-
-
 
 cd /home/gitlab-runner
 
@@ -73,6 +81,21 @@ export SANDBOX_LOAD_GITLAB_ENV="`load_bool "$SANDBOX_LOAD_GITLAB" "true"`"
 
 check_env
 create_user_env
+
+SSH_PORT=22
+if echo "$SSH_CONNECT" | grep ':[0-9]\+$'; then
+    SSH_PORT="`echo "$SSH_CONNECT" | sed 's/^.*:\([0-9]\+\)$/\1/'`"
+    SSH_CONNECT="`echo "$SSH_CONNECT" | sed 's/:[0-9]\+$//'`"
+    SSH_PORT="`expr "$SSH_PORT" + 0`"
+    if [ "$SSH_PORT" -lt 1 -o "$SSH_PORT" -gt "65535" ]; then
+        echo "Error: invalid ssh port" 1>&2
+        exit 1
+    fi
+fi
+
+SSH_HOST="`echo "$SSH_CONNECT" | sed 's/^.*@//'`"
+SSH_USER="`echo "$SSH_CONNECT" | sed 's/@[^@]*$//'`"
+
 
 runner_args=()
 
@@ -82,7 +105,7 @@ if [ -n "$RUNNER_NAME" ]; then
     runner_args+=(--name "$RUNNER_NAME")
 fi
 
-runner_args+=(--ssh-user sandbox --ssh-host sandbox --ssh-port 22)
+runner_args+=(--ssh-user "$SSH_USER" --ssh-host "$SSH_HOST" --ssh-port "$SSH_PORT")
 
 runner_args+=(--ssh-password "$SSH_PASSWORD")
 
